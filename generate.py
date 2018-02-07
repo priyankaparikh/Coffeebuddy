3eb43b799e512cf982643b8fe338dc308d925c26
vagrant <vagrant@vagrant.vm>
2018-02-07 00:36:55 +0000

    begin seeding data

--- generate.py
+++ generate.py
@@ -1,4 +1,4 @@
-from faker import Faker, Hipster
+from faker import Faker
 import random
 fake = Faker()
 text_file = open("seed_data/user_data.txt", "w")
@@ -9,6 +9,7 @@ def generate_users(n):
     email_providers = ['gmail','yahoo','aol','hotmail','fastmail']
 
     for i in range(1, n + 1):
+        user_id = i
         fname = fake.first_name()
         lname = fake.last_name()
         email = fname + lname + '@' + random.choice(email_providers) + '.com'
@@ -17,9 +18,9 @@ def generate_users(n):
         date_of_birth = fake.date_between(start_date="-40y", end_date="-16y")
         zipcode = fake.zipcode()
         phone = fake.phone_number()
-        one_word = fake.Hipster.word(ext_word_list=None)
-        data1 = fname + "|" + lname + "|" + email + "|" + user_name + "|" + password 
-        data2 = "|" + str(date_of_birth) + "|" + zipcode + "|" + phone + "|" + one_word + "|"
+        one_word = fake.word(ext_word_list=None)
+        data1 = str(i) + "|" + fname + "|" + lname + "|" + email + "|" + user_name + "|" + password 
+        data2 = "|" + str(date_of_birth) + "|" + zipcode + "|" + phone + "|" + one_word
         data = data1 + data2 + "\n"
         text_file.write(data)
         print data 
--- seed.py
+++ seed.py
@@ -4,21 +4,27 @@ from sqlalchemy import func
 from models import connect_to_db, db
 from server import app
 from models import User
+from models import Book_genre
+from models import Movie_genre
+from models import Music_genre
+# from models import Food_habit
+# from models import Fav_cuisine
 from faker import Faker
 
 def load_users():
     """Load users from static/user_data.txt into database."""
 
-    print "Users"
+    print "User"
     User.query.delete()
 
     # Read u.user file and insert data
-    for row in open("seed_data/user_data.txt")
+    for row in open("seed_data/user_data.txt"):
         row = row.rstrip()
         #is this pythonic ?
-        fname, lname, email, user_name, password, date_of_birth, zipcode, phone ,one_word = row.split("|")
+        user_id, fname, lname, email, user_name, password, date_of_birth, zipcode, phone, one_word = row.split("|")
 
-        user = User(fname=fname,
+        user = User(user_id=user_id,
+                    fname=fname,
                     lname=lname,
                     email=email,
                     user_name=user_name,
@@ -35,29 +41,127 @@ def load_users():
     db.session.commit()
 
 
-def load_user_interests():
+def load_books():
+    """Load books from book_genre_data into database."""
 
+    print "Book_genre"
+    User.query.delete()
+
+    for row in open("seed_data/book_genre_data.txt"):
+        row = row.rstrip()
+        book_genre_id, book_genre_name = row.split("|")
+
+        book = Book_genre(book_genre_id=book_genre_id,
+                          book_genre_name=book_genre_name)
+
+        db.session.add(book)
+
+    db.session.commit()
 
 
 def load_movies():
-    """Load movies from u.item into database."""
+    """Load movies from movie_genre_data into database."""
 
+    print "Movie_genre"
+    User.query.delete()
 
-def load_ratings():
-    """Load ratings from u.data into database."""
+    for row in open("seed_data/movie_genre_data.txt"):
+        row = row.rstrip()
+        movie_genre_id, movie_genre_name = row.split("|")
 
+        movie = Movie_genre(movie_genre_id=movie_genre_id,
+                          movie_genre_name=movie_genre_name)
 
-def set_val_user_id():
-    """Set value for the next user_id after seeding database"""
+        db.session.add(movie)
 
-    # Get the Max user_id in the database
-    result = db.session.query(func.max(User.user_id)).one()
-    max_id = int(result[0])
-
-    # Set the value for the next user_id to be max_id + 1
-    query = "SELECT setval('users_user_id_seq', :new_id)"
-    db.session.execute(query, {'new_id': max_id + 1})
     db.session.commit()
+
+
+def load_music():
+    """Load music from music_genre_data into database."""
+
+    print "music_genre"
+    User.query.delete()
+
+    for row in open("seed_data/music_genre_data.txt"):
+        row = row.rstrip()
+        music_genre_id, music_genre_name = row.split("|")
+
+        music = Music_genre(music_genre_id=music_genre_id,
+                          music_genre_name=music_genre_name)
+
+        db.session.add(music)
+
+    db.session.commit()
+
+
+def load_food_habits():
+    """Load food_habits from food_habit_data into database."""
+
+    print "Food_habit"
+    User.query.delete()
+
+    for row in open("seed_data/food_habit_data.txt"):
+        row = row.rstrip()
+        food_habit_id, food_habit_name = row.split("|")
+
+        habit = Food_habit(food_habit_id=food_habit_id,
+                          food_habit_name=food_habit_name)
+
+        db.session.add(habit)
+
+    db.session.commit()
+
+
+def load_cuisines():
+    """Load  from fav_cuisine_data into database."""
+
+    print "Fav_cuisine"
+    User.query.delete()
+
+    for row in open("seed_data/fav_cuisine_data.txt"):
+        row = row.rstrip()
+        fav_cuisine_id, fav_cuisine_name = row.split("|")
+
+        cuisine = Fav_cuisine(fav_cuisine_id=fav_cuisine_id,
+                          fav_cuisine_name=fav_cuisine_name)
+
+        db.session.add(cuisine)
+
+    db.session.commit()
+
+
+def load_cuisines():
+    """Load  from book_genre_data into database."""
+
+    print "Fav_cuisine"
+    User.query.delete()
+
+    for row in open("seed_data/fav_cuisine_data.txt"):
+        row = row.rstrip()
+        fav_cuisine_id, fav_cuisine_name = row.split("|")
+
+        cuisine = Fav_cuisine(fav_cuisine_id=fav_cuisine_id,
+                          fav_cuisine_name=fav_cuisine_name)
+
+        db.session.add(cuisine)
+
+    db.session.commit()
+# def load_ratings():
+#     """Load ratings from u.data into database."""
+
+
+# def set_val_user_id():
+#     """Set value for the next user_id after seeding database"""
+
+#     # Get the Max user_id in the database
+#     result = db.session.query(func.max(User.user_id)).one()
+#     max_id = int(result[0])
+
+#     # Set the value for the next user_id to be max_id + 1
+#     query = "SELECT setval('users_user_id_seq', :new_id)"
+#     db.session.execute(query, {'new_id': max_id + 1})
+#     db.session.commit()
 
 
 if __name__ == "__main__":
@@ -68,6 +172,8 @@ if __name__ == "__main__":
 
     # Import different types of data
     load_users()
+    load_books()
     load_movies()
-    load_ratings()
-    set_val_user_id()
+    load_music()
+    # load_food_habits()
+    # set_val_user_id()
