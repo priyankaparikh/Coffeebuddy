eb1900c9154a76089e5100beb3f4a75776b706e2
vagrant <vagrant@vagrant.vm>
2018-03-12 22:39:23 +0000

    added new tests

--- tests.py
+++ tests.py
@@ -1,34 +1,36 @@
-""" This module tests for
-- testing the the routes
-- testing if the templates rendered correctly
-- testing the db for correct models
-- testing if the page got the correct info
-    from the db
+""" Module to test the CoffeeBuddy application.
 """
-
 import unittest
 from server import app
 from models import db, example_data, connect_to_db
+from yelper import *
 
 #####################################################################
 
+class CoffeshopTests(unittest.TestCase):
+    """Tests the Yelp API for correct responses."""
 
-class CoffeeTests(unittest.TestCase):
-    """Tests basic get routes for Coffebuddy Web app site."""
-
-    def setUp(self):
-        """ Do this before any test."""
+    def setup(self):
+        """ Base setup for the Yelp Api. """
         self.client = app.test_client()
         app.config['TESTING'] = True
 
-    def test_home(self):
-        result = self.client.get("/")
-        self.assertIn("<h1>CoffeeBuddy</h1>", result.data)
+        def mock_request(host, path, api_key, url_params=None):
+            return "[{u'alias': u'coffee', u'title': u'Coffee & Tea'}]"
 
-    def test_login1(self):
-        result = self.client.get("/login")
-        self.assertIn("<h1> Login Form</h1>", result.data)
+        requests.request = mock_request
 
+        def mock_search(location):
+            return "[{u'alias': u'coffee', u'title': u'Coffee & Tea'}]"
+
+        search = mock_search
+
+
+        def test_mock_request(self):
+            self.assertIn('coffee', result)
+
+        def test_mock_request(self):
+            self.assertIn('coffee', result)
 
 class CoffeebuddyTestsDatabase(unittest.TestCase):
     """Flask tests that use the database."""
@@ -51,132 +53,87 @@ class CoffeebuddyTestsDatabase(unittest.TestCase):
         # drop the db
         db.drop_all()
 
-    def test_user(self):
-        """ Test the user profile page for user_info."""
+    def test_home(self):
+        result = self.client.get("/")
+        self.assertIn('<div class="carousel-caption">', result.data)
 
-        result = self.client.get("/user_info")
-        self.assertIn("", result.data)
+    def test_login_page(self):
+        result = self.client.get("/login")
+        self.assertIn('<div class="form-group">', result.data)
 
-    def test_user_interests(self):
-        """ Test the user profile page for user_interests."""
-
-        result = self.client.get("user/info")
-        self.assertIn("", result)
-
-    def test_book_genres_list(self):
-        """ Test register page for book_genres from db."""
+    def test_registration_page(self):
+        """ Test register page for information from db."""
 
         result = self.client.get("/register")
+        # Test register page for book_genres from db.
         self.assertIn("Horror", result.data)
-
-    def test_movie_genres_list(self):
-        """ Test register page for movie_genres from db."""
-
-        result = self.client.get("/register")
+        # Test register page for movie_genres from db
         self.assertIn("Action", result.data)
-
-    def test_music_genres_list(self):
-        """ Test register page for music_genres from db."""
-
-        result = self.client.get("/register")
+        # Test register page for music_genres from db.
         self.assertIn("Metal", result.data)
-
-    def test_food_habit_list(self):
-        """ Test register page for food_habits from db."""
-
-        result = self.client.get("/register")
+        # Test register page for food_habits from db.
         self.assertIn("Vegan", result.data)
-
-    def test_fav_cuisine_list(self):
-        """ Test register page for fav_cuisines from db."""
-
-        result = self.client.get("/register")
+        # Test register page for fav_cuisines from db.
         self.assertIn("Italian", result.data)
-
-    def test_hobby1(self):
-        """ Test register page for hobbies from db."""
-
-        result = self.client.get("/register")
+        # Test register page for hobbies from db.
         self.assertIn("Sewing", result.data)
-
-    def test_political_view_list(self):
-        """ Test register page for political_views from db."""
-
-        result = self.client.get("/register")
+        # Test register page for political_views from db.
         self.assertIn("Democrat", result.data)
-
-    def test_religion_list(self):
-        """ Test register page for religions from db."""
-
-        result = self.client.get("/register")
+        # Test register page for religions from db.
         self.assertIn("Hindu", result.data)
-
-    def test_outdoor_list(self):
-        """ Test register page for outdoor activities from db."""
-
-        result = self.client.get("/register")
+        # Test register page for outdoor activities from db.
         self.assertIn("hiking", result.data)
-
-    def test_login2(self):
-        """ Test the the login page for the correct path."""
-
-        result = self.client.post("/login",
-                                data={"email": "eforman@gmail.com",
-                                    "password": "Whimsical"},
-                                follow_redirects=True)
-        self.assertIn("<h1>Trip Form</h1>", result.data)
-
-    def test_register1(self):
-        """ Test the registeration page."""
-
-        result = self.client.get("/register")
-        self.assertIn("<p>User name</p>", result.data)
-
-    def test_register2(self):
-        """ Test the registeration process."""
-        result = self.client.post("/register",
-                                    data={
-                                    "fname": "Jane",
-                                    "lname": "Doe",
-                                    "email": "jdoe@gmail.com",
-                                    "user_name": "jdo",
-                                    "password": "tamtam",
-                                    "date_of_birth": "26-5-1978",
-                                    "zipcode": "97678",
-                                    "phone": "404-995-0900",
-                                    "one_word": "joke",
-                                    "Preferred book genre": 1,
-                                    "Preferred movie genre": 1,
-                                    "Preferred music genre": 2,
-                                    "Food habits": 1,
-                                    "Preferred cuisine type": 2,
-                                    "Favorite hobby": 1,
-                                    "Political ideology": 1,
-                                    "Religious ideology": 1,
-                                    "Favorite Outdoor activity": 1,
-                                    "profile_picture": ""},
-                                    follow_redirects=True)
-        self.assertIn("<h1>Trip Form</h1>", result.data)
-
-    def test_plan_trip_page(Self):
-        """ test if the plan trip page loads correctly """
-
-        result = self.client.get("/plan_trip")
-
-        self.assertIn("<p> What time would you like to get coffee? </p>")
+        # Test register page HTML
+        self.assertIn("<h2>New User Registeration<h2>", result.data)
 
 
-    def test_plan_trip(self):
-        """ test the input of user and check for the redirect."""
 
-        result = self.client.post("/plan_trip",
-                                data={
-                                "triptime": "2018-02-15 22:20:21.313644",
-                                "pincode": 95111},
-                                follow_redirects=True)
+    # def test_register2(self):
+    #     """ Test the registeration process."""
+    #     result = self.client.post("/register",
+    #                                 data={
+    #                                 "fname": "Jane",
+    #                                 "lname": "Doe",
+    #                                 "email": "jdoe@gmail.com",
+    #                                 "user_name": "jdo",
+    #                                 "password": "tamtam",
+    #                                 "date_of_birth": "26-5-1978",
+    #                                 "zipcode": "97678",
+    #                                 "phone": "404-995-0900",
+    #                                 "one_word": "joke",
+    #                                 "Preferred book genre": 1,
+    #                                 "Preferred movie genre": 1,
+    #                                 "Preferred music genre": 2,
+    #                                 "Food habits": 1,
+    #                                 "Preferred cuisine type": 2,
+    #                                 "Favorite hobby": 1,
+    #                                 "Political ideology": 1,
+    #                                 "Religious ideology": 1,
+    #                                 "Favorite Outdoor activity": 1,
+    #                                 "profile_picture": ""},
+    #                                 follow_redirects=True)
+    #     self.assertIn("<h1>Trip Form</h1>", result.data)
 
-        self.assertIn("<p> Here is a list of your potential Matches</p>",
-                    result.data)
+    # def test_plan_trip_page(self):
+    #     """ test if the plan trip page loads correctly """
+    #
+    #     result = self.client.get("/plan_trip")
+    #
+    #     self.assertIn("<p> What time would you like to get coffee? </p>")
+    #
+    #
+    # def test_plan_trip(self):
+    #     """ test the input of user and check for the redirect."""
+    #
+    #     result = self.client.post("/plan_trip",
+    #                             data={
+    #                             "triptime": "2018-02-15 22:20:21.313644",
+    #                             "pincode": 95111},
+    #                             follow_redirects=True)
+    #
+    #     self.assertIn("<p> Here is a list of your potential Matches</p>",
+    #                 result.data)
+
 
 ####################################################################
 
